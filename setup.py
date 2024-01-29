3ea611c4f285a117126f91af6b32430cefed9e94
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2024-01-29 14:27:47 +0400

    [RM-84612] Updated dependencies

--- setup.py
+++ setup.py
@@ -123,8 +123,8 @@ setup(
     license='Apache License 2.0',
     python_requires='>=3.7',
     install_requires=[
-        'th2-grpc-common>=3,<4',
-        'mypy-protobuf==3.2'
+        'th2-grpc-common==4.4.0rc1',
+        'mypy-protobuf==3.4'
     ],
     packages=packages,
     package_data=package_data,
