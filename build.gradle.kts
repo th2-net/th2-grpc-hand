9accd28b670870b3d0143683c176e48aedef6c21
Nikita Smirnov <46124551+Nikita-Smirnov-Exactpro@users.noreply.github.com>
2024-04-08 15:48:28 +0400

    [RM-84612] Updated libraries & workflows, th2-gradle-plugin (#37)

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+
+    val th2PluginVersion = "0.0.4"
+    id("com.exactpro.th2.gradle.grpc") version th2PluginVersion
+    id("com.exactpro.th2.gradle.publish") version th2PluginVersion
+}
+
+group = "com.exactpro.th2"
+version = project.properties["release_version"].toString()
+
+configurations.all {
+    resolutionStrategy.cacheChangingModulesFor(0, "seconds")
+    resolutionStrategy.cacheDynamicVersionsFor(0, "seconds")
+}
+
+th2Grpc {
+    service.set(true)
+}
+
+dependencies {
+    api("com.exactpro.th2:grpc-common:4.4.0-dev")
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "Sonatype_snapshots"
+        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    }
+    maven {
+        name = "Sonatype_releases"
+        url = uri("https://s01.oss.sonatype.org/content/repositories/releases/")
+    }
+}
